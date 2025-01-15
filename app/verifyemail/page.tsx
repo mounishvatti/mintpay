182e19f6b9f1d379f6fa219999f9c098fc8aa71f
mounishvatti2002 <mounishvatti2002@gmail.com>
2025-01-15 09:36:35 +0000

    minor ui fix

--- app/verifyemail/page.tsx
+++ app/verifyemail/page.tsx
@@ -12,23 +12,23 @@ import { REGEXP_ONLY_DIGITS } from "input-otp";
 
 export default function ConfirmEmailPage() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-            <div className="border-2 border-dashed border-violet-700 rounded-lg p-8 m-4 w-full max-w-md shadow-md gap-4">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-violet-900">
+            <div className="rounded-lg p-8 m-4 w-full max-w-md shadow-md gap-4 bg-white">
                 {/* Logo Section */}
                 <div className="flex justify-center gap-2 md:justify-start">
                     <a href="#" className="flex items-center gap-2 font-medium">
-                        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-                            <BadgeIndianRupee className="size-6" />
-                        </div>
-                        <span className="text-2xl font-sans text-violet-700 font-bold italic">
-                            Rupay
+                        {/* <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+                            <BadgeIndianRupee className="size-5" />
+                        </div> */}
+                        <span className="text-2xl font-sans text-violet-600 font-bold italic">
+                            rupay
                         </span>
                     </a>
                 </div>
 
                 {/* OTP Section */}
                 <div className="mt-6">
-                    <p className="text-center text-lg font-normal font-sans text-slate-900 p-2 italic">
+                    <p className="text-center text-md font-normal font-sans text-slate-900 p-2">
                         Enter the code sent to your email
                     </p>
 
