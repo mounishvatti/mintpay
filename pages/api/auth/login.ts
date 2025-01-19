e520a4e696c1595415db4557deeb4bd0b9884447
Mounish Vatti <mounishvatti2002@gmail.com>
2025-01-19 12:54:12 +0530

    added samesite strict

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -52,7 +52,7 @@ export default async function handler(
     // Store the token in cookies (optional, for session-based auth)
     res.setHeader(
       "Set-Cookie",
-      `token=${token}; HttpOnly; Secure; Path=/; Max-Age=3600`,
+      `token=${token}; HttpOnly; Secure; Path=/; Max-Age=3600 SameSite=Strict`,
     );
     
       return res.status(200).send({
