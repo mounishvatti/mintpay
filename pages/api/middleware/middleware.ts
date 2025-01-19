200b82cf5b246777b27e4e46cb376421ae9cb266
Mounish Vatti <mounishvatti2002@gmail.com>
2025-01-19 12:54:39 +0530

    added types for request

--- pages/api/middleware/middleware.ts
+++ pages/api/middleware/middleware.ts
@@ -1,7 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
+
+interface AuthenticatedRequest extends NextApiRequest {
+  user?: any; // Replace 'any' with the actual type of your decoded JWT payload
+}
+
 // Custom middleware to verify JWT token
-export async function verifyJWT(req: NextApiRequest, res: NextApiResponse, next: Function) {
+export async function verifyJWT(req: AuthenticatedRequest, res: NextApiResponse, next: Function) {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1]; // Get token from cookies or authorization header
   
   if (!token) {
@@ -11,7 +16,6 @@ export async function verifyJWT(req: NextApiRequest, res: NextApiResponse, next:
   try {
     // Verify the token
     const decoded = jwt.verify(token, process.env.JWT_USER_SECRET!); // Ensure that JWT_USER_SECRET is set in your environment variables
-    //@ts-ignore
     req.user = decoded; // Add the decoded token data (e.g., userId) to the request object
     
     return next(); // Proceed to the next handler
